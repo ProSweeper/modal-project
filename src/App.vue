81e6d19c2508e8ea13663afc52ae1bc464cbeedc
ProSweeper <dylan.doucet28@gmail.com>
2023-04-06 17:32:56 +0000

    start/finsih vue modal project

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <!-- if the following is not present the default slot will render -->
+      <h1>GiveAway</h1>
+      <p>come get it obama</p>
+    </Modal> 
+  </teleport>
+
+  <teleport to=".modals" v-if="showModal2">
+    <Modal @close="toggleModal2">
+      <template v-slot:links>
+        <a href="#">sign up again</a>
+        <a href="#">more info on stuff</a>
+      </template>
+      <!-- if the following is not present the default slot will render -->
+      <h1>YEYE</h1>
+      <p>what it doooooo</p>
+    </Modal> 
+  </teleport>
+  
+  <button @click="handleClick">click me</button>
+  <br>
+  <button @click.alt="toggleModal">Toggle Modal (alt)</button>
+  <button @click.alt="toggleModal2">Toggle Modal 2 (alt)</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/ModalComp.vue"
 
 export default {
   name: 'App',
+  data() {
+    return {
+      title: "My First Vue App",
+      header: "Hello There",
+      string: "heeyyyoooo",
+      showModal: false,
+      showModal2: false,
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add("active")
+      this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2
+    }
+  },
   components: {
-    HelloWorld
+    Modal,
   }
 }
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/ModalComp.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <!-- will only display if there is no HTML in the component -->
+      <slot>Default Content</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      closeModal() {
+        this.$emit('close')
+      }
+    }
+  }
+</script>
+
+<style>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white; 
+    border-radius: 10px;
+  }
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+  }
+  .modal.sale {
+    background: crimson;
+    color: white;
+  }
+</style>
